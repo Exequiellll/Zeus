d895a020b8daae33589ec3d6ab4d7cec50465e26
Exequiel Co <ejayrco@gmail.com>
2026-05-05 13:54:25 +0800

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('clickMe');
+const counter = document.getElementById('counter');
+let clicks = 0;
+
+button.addEventListener('click', () => {
+    clicks++;
+    counter.textContent = `Clicks: ${clicks}`;
+});
